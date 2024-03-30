5c0f87546fa54f8ff0402ef5d1f538ef00c506dc
fsantos93x@gmail.com <fsantos93x@gmail.com>
2024-03-29 21:50:50 -0300

    updated class

---
+++ src/components/PostCard/aulaspast.jsx
@@ -0,0 +1,51 @@
+
+     
+      //LIFECYCLE METHOS, SETTIMEOUT
+      // setTimeout(() => {
+      //   this.setState({
+      //     posts: [
+      //       {
+      //         id: 1,
+      //         title: 'O titulo 1',
+      //         body: 'O corpo 1'
+      //       },
+    
+      //       {
+      //         id: 2,
+      //         title: 'O titulo 2',
+      //         body: 'O corpo 2'
+      //       },
+    
+      //       {
+      //         id: 3,
+      //         title: 'O titulo 3',
+      //         body: 'O corpo 3'
+      //       }
+      //     ]
+      //   })
+        
+      // }, 5000);
+
+      
+
+       // handleTimeount = () => {
+    //   const { posts, counter } = this.state;
+    //   posts[0].title = "O titulo mudou"
+
+    //   this.timeoutUpdate = setTimeout(() => { 
+    //     this.setState({ posts, counter: counter + 1 });
+    //   }, 40)
+    // }
+  
+
+  // handlePClick =() => {
+  //   this.setState({ name: 'Junior'});
+  //   // const {name} = this.state;
+  //   // console.log(`<p> clicado ${name}`)
+  // }
+
+  // handleAClick = (event)  => {
+  //   event.preventDefault();
+  //   const {counter} = this.state;
+  //   this.setState({ counter: counter + 1});
+  // }
