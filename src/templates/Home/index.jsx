9067443e94e12b801879ede738aa6baa070cfca1
fsantos93x@gmail.com <fsantos93x@gmail.com>
2024-04-07 01:43:22 -0300

    Hooks demonstrate

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,109 +1,28 @@
 import './styles.css';
-
 import { Component } from 'react';
 
-import { Posts } from "../../components/Posts";
-import { loadPosts } from "../../utils/load-posts";
-import {Button} from '../../components/Button';
-import { TextInput } from '../../components/TextInput';
-
 export class Home extends Component {
+  state = {
+    counter: 0
+  }
 
-    state = {
-      posts: [],
-      allPosts: [],
-      page: 0,
-      postsPerPage: 10,
-      searchValue: ''
-    };
-
-
-    async componentDidMount() {
-      await this.loadPosts();
-    
-    }
-
-    componentDidUpdate () {
-      console.log('PROP', this.props)
-    }
-
-    loadPosts = async () => {
-      const { page,  postsPerPage } = this.state;
-
-      const postsAndPhotos = await loadPosts();
-      this.setState({
-         posts: postsAndPhotos.slice(page, postsPerPage),
-         allPosts: postsAndPhotos, 
-        });
-    }
-
-    
-    loadMorePosts = () => {
-      const {
-        page,
-        postsPerPage,
-        allPosts,
-        posts
-      } = this.state;
-      const nextPage = page + postsPerPage;
-      const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
-      posts.push(...nextPosts);
-
-      this.setState({ posts, page: nextPage })
-
-    }
-
-    handleChange = (e) => {
-      const { value } = e.target;
-      this.setState({ searchValue: value })
+  handleClick = () => {
+    this.setState(
+      (prevState, prevProps) => {
+        console.log('PROPS',prevProps.numberToIncrement);
+        return {counter: prevState.counter + prevProps.numberToIncrement}
+      },
+      () => {
+        console.log('POST', this.state.counter);
+      });
     }
 
   render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-    const filteredPosts = !!searchValue ? 
-    allPosts.filter(post => {
-      return post.title.toLowerCase().includes(searchValue.toLowerCase()
-      );
-    })
-    : posts;
-
     return (
-      <section className='container'>
-
-        {!!searchValue &&
-        <>
-        <h1>Search value: {searchValue}</h1> <br />
-        </> 
-        }
-
-        <TextInput searchValue={searchValue} handleChange={this.handleChange} />
-     
-      <br /><br />
-        {filteredPosts.length > 0 && (
-        <Posts posts={filteredPosts} />
-        )}
-
-        {filteredPosts.length === 0 && (
-          <p>Não existem posts</p>
-        )}
-
-        <div className="button-container">
-          {!searchValue && (
-             <Button 
-             text = "Load more posts"
-             onClick= {this.loadMorePosts}
-             disabled={noMorePosts}
-             />
-          )}
-       
-        </div>
-        
-      </section>
-
-          
-        );
+      <div className="container">
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleClick}>Increment</button>
+      </div>
+    )
   }
 }
-
